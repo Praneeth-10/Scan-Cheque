03362dc3ae47111fa687a89a19f36986b902ac08
noobprogramr <sp.parasa@gmail.com>
2024-04-08 16:47:54 -0400

    Adding necessary ML kit Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,9 +39,16 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //MlKit
+    implementation("androidx.camera:camera-camera2:1.3.2")
+    implementation("androidx.camera:camera-lifecycle:1.3.2")
+    implementation("androidx.camera:camera-view:1.3.2")
+//    implementation("com.google.mlkit:ocr:16.0.0")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
 }
